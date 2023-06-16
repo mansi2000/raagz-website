c6f2ce2c01ba1f152e0e3708b27059a3a0b5bf8d
mansi <mansibansal2000@gmail.com>
2023-06-17 03:00:20 +0530

    added about us page

--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,32 @@
 import Layout from '../components/Layout';
+import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
 
-const AboutPage = () => {
+builder.init("86f5d179d02a4930900a5317a3da5d45");
+
+export async function getStaticProps() {
+  // Fetch the builder content for the '/about' page
+  const page = await builder
+    .get('page', {
+      userAttributes: {
+        urlPath: '/about',
+      },
+    })
+    .toPromise();
+
+  return {
+    props: {
+      page: page || null,
+    },
+    revalidate: 5,
+  };
+}
+
+
+const AboutPage = ({ page }) => {
   return (
     <Layout>
-      <div>
-        <h1>About Us</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida tellus elit, vitae
-          aliquam mauris dignissim non. Sed congue convallis enim, non venenatis erat consequat vel.
-          Sed ut est nec mi suscipit lacinia. Phasellus interdum, felis a commodo tristique, mauris
-          lorem tristique eros, sit amet sagittis odio mi vel ipsum. Curabitur mollis purus a justo
-          interdum, ac fringilla lacus facilisis. Curabitur auctor finibus justo, nec blandit ex
-          aliquam id. In eleifend eros non risus malesuada, id malesuada eros aliquam.
-        </p>
-        <p>
-          Integer eget neque ac orci ullamcorper venenatis. Suspendisse potenti. Phasellus egestas,
-          tortor in consectetur pharetra, erat velit luctus sem, et sollicitudin ipsum felis at
-          purus. Vestibulum bibendum est nec lectus condimentum, vitae facilisis leo bibendum.
-          Curabitur vestibulum velit eu orci scelerisque, at blandit mi dignissim. Maecenas
-          ullamcorper turpis at lacinia vestibulum. Curabitur non finibus quam.
-        </p>
-        <p>
-          Nullam tristique velit eu eros dignissim, ac dignissim enim commodo. Sed nec ligula eget
-          velit lacinia pulvinar id vitae lacus. Vivamus aliquam, odio sed porttitor egestas, nibh
-          ligula ultricies dolor, ac rutrum tortor justo sed enim. Mauris sed metus elit. Aliquam
-          erat volutpat. Aliquam maximus, felis id volutpat cursus, tellus mi sollicitudin massa,
-          sit amet ullamcorper nibh lacus sed felis. Nulla facilisi.
-        </p>
-      </div>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={page} />
     </Layout>
   );
 };
