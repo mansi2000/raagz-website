ce93e4c8264f3a210088f17b54043c55cef79def
mansi2000 <mansibansal2000@gmail.com>
2024-03-21 20:34:00 +0530

    Footer content uploaded

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,11 @@
+// Footer.jsx
+import React from 'react';
 import Link from 'next/link';
+import styles from '../styles/Footer.module.css' // Importing the CSS module
 
 const Footer = () => {
-    return (
-      <footer>
+  return (
+    <footer className={styles.footer}>
         <div>
           <ul>
             <li>
@@ -12,48 +15,19 @@ const Footer = () => {
               <Link href="/privacy">Privacy Policy</Link>
             </li>
             <li>
-              <Link href="/refund">Refund Policy</Link>
+              <Link href="/contact">Contact Us</Link>
             </li>
           </ul>
         </div>
-        <div>
-          <p className="centered-text">© 2024 Madyam Music Private Limited. All rights reserved.</p>
-        </div>
-        <style jsx>{`
-        footer {
-          background-color: #4A4A4A;
-          padding: 20px;
-        }
+      <div className={styles.socialLinks}>
+        <a href="https://www.instagram.com/raagz.in/" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://www.linkedin.com/company/raagz/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </div>
+      <div>
+          <p>© 2024 Madyam Music Private Limited. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
 
-        div ul {
-          list-style: none;
-          display: flex;
-          justify-content: center;
-        }
-
-        div ul li {
-          margin-right: 20px;
-        }
-
-        .div-link {
-          color: #333;
-          text-decoration: none;
-          font-weight: bold;
-        }
-
-        .div-link:hover {
-          color: #666;
-        }
-
-        .centered-text {
-          text-align: center;
-          margin-left: auto;
-          margin-right: auto;
-        }
-      `}</style>
-      </footer>
-    );
-  };
-  
-  export default Footer;
-  
+export default Footer;
